77662d4877d2cf4ccc50aa4a4ff843b490ab797a
mistertandon <enggparveshtandon@gmail.com>
2020-04-24 14:31:44 +0530

    feat: Why do we need OOPs concepts
    Develop code to demonstrate need of OOPs.

---
+++ oops-01.py
@@ -0,0 +1,17 @@
+class Employee:
+    pass
+
+
+emp_i = Employee()
+emp_ii = Employee()
+
+emp_i.name = 'parvesh'
+emp_i.last_name = 'tandon'
+emp_i.email = "{}{} @company.com".format(emp_i.name, emp_i.last_name)
+
+emp_ii.name = 'jiyanshi'
+emp_ii.last_name = 'tandon'
+emp_ii.email = "{}{} @company.com".format(emp_ii.name, emp_ii.last_name)
+
+print(emp_i)
+print(emp_ii)
