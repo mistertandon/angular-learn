beecac3590323b4e82e24f5a1af0bf96d323b7a5
mistertandon <enggparveshtandon@gmail.com>
2022-10-29 14:55:26 +0530

    feat: class component lifecycle methods while mounting

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Parent from './components/Parent'
+
+const App = () => {
+
+  return (
+
+    <div className="App">
+      <Parent />
+    </div>
+
+  );
+}
+
+export default App;
